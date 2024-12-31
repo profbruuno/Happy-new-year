d0f8b09aded766514b23c9486502f63d59780386
profbruuno <96679152+profbruuno@users.noreply.github.com>
2024-12-31 22:01:30 +0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Countdown Timer Script
 document.addEventListener("DOMContentLoaded", function() {
     const targetDate = new Date('2025-01-01T00:00:00+03:00'); // 12:00 AM EAT
-    
+
     function updateCountdown(timerId) {
         const countdownTimer = document.getElementById(timerId);
         const timer = setInterval(() => {
@@ -12,23 +12,23 @@ document.addEventListener("DOMContentLoaded", function() {
             const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            const milliseconds = Math.floor((distance % 1000) / 10);
 
             countdownTimer.innerHTML = `
                 <span>${days}d </span>
                 <span>${hours}h </span>
                 <span>${minutes}m </span>
-                <span>${seconds}s</span>
+                <span>${seconds}s </span>
+                <span>${milliseconds}ms</span>
             `;
 
             if (distance < 0) {
                 clearInterval(timer);
                 countdownTimer.innerHTML = "Congratulations upon reaching 2025!";
             }
-        }, 1000);
+        }, 10);
     }
 
     updateCountdown('top-timer');
-    updateCountdown('middle-timer');
-    updateCountdown('bottom-timer-1');
-    updateCountdown('bottom-timer-2');
+    updateCountdown('bottom-timer');
 });
